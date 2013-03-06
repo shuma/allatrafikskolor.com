5963ea53482abb2f6b055d4a03271fbe07c74472
Usman Rajab <usman.rajab@gmail.com>
2013-03-06 18:54:15 +0100

    fuck you heroku

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,4 +1,4 @@
-ENV['http://agmdni11:g4uollxoeowhzt7d@apricot-4753209.us-east-1.bonsai.io'] = ENV['BONSAI_URL']
+ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
 
 # Optional, but recommended: use a single index per application per environment.
 # Caveat: This convention not be entirely supported throughout Tire's API.
