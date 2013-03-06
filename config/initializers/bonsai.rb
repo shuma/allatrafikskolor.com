77b33144dc4a180ab97c3443bd33ec2ccfe51fa0
Usman Rajab <usman.rajab@gmail.com>
2013-03-06 19:04:30 +0100

    Alright, so we are friends again

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,3 +1,5 @@
+# Link to my stackoverflow question: http://stackoverflow.com/questions/15253773/config-tire-to-work-with-bonsai-elasticsearch-heroku-add-on/15254656#15254656
+# how to config bonsai with tire : https://gist.github.com/nz/2041121
 ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
 
 # Optional, but recommended: use a single index per application per environment.
