328192154e062e362ed9578162ae99010d0e2edc
Usman Rajab <usman.rajab@gmail.com>
2013-03-06 18:02:45 +0100

    bonsai fuck you

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,7 +1,11 @@
-ENV['http://agmdni11:g4uollxoeowhzt7d@apricot-4753209.us-east-1.bonsai.io'] = ENV['BONSAI_URL']
-
-# Optional, but recommended: use a single index per application per environment.
-# Caveat: This convention not be entirely supported throughout Tire's API.
-app_name = Rails.application.class.parent_name.underscore.dasherize
-app_env = Rails.env
-INDEX_NAME = "#{app_name}-#{app_env}"
+Tire.configure { logger 'elasticsearch.log', :level => 'debug' }
+if ENV['BONSAI_INDEX_URL']
+  Tire.configure do
+    url "http://agmdni11:g4uollxoeowhzt7d@apricot-4753209.us-east-1.bonsai.io"
+  end
+  BONSAI_INDEX_NAME = ENV['BONSAI_INDEX_URL'][/[^\/]+$/]
+else
+  app_name = Rails.application.class.parent_name.underscore.dasherize
+  app_env = Rails.env
+  BONSAI_INDEX_NAME = "#{app_name}-#{app_env}"
+end
