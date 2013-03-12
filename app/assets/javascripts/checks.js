66df93b41f46527f2e93bebe6fe732a72e8a49ed
Usman Rajab <usman.rajab@gmail.com>
2013-03-12 17:20:09 +0100

    jQuery error

--- app/assets/javascripts/checks.js
+++ app/assets/javascripts/checks.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('resCheckLink').click(function(){
 		$('resCheck label label-success').removeClass('label-success');
-		
+		$('resCheck label').addClass('selected');
 	});
 });
