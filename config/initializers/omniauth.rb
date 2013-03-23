5767db4b521265f0afbe379cc2b9e755eff83fc1
Usman Rajab <usman.rajab@gmail.com>
2013-03-23 19:30:27 +0100

    f

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '294980710610666', '865e81cb43e2fd4fbfebb8d7e951ad13', scope: 'email,publish_actions'
+  provider :facebook, '294980710610666', '865e81cb43e2fd4fbfebb8d7e951ad13', 
+  scope: 'email,publish_actions'
 end
 
