839bafb33bdb486495b32d5dc369c5752393d9b4
Usman Rajab <usman.rajab@gmail.com>
2013-03-25 14:03:18 +0100

    kj

--- app/assets/javascripts/checks.js
+++ app/assets/javascripts/checks.js
@@ -1,7 +1,6 @@
 $(function(){
     $('.resCheckLink').click(function(e){
-        e.preventDefault();
-        $('.resCheck.label').removeClass('.label-success');
-        $(this).parent().addClass('.label-success')
+        $('.resCheck.label').removeClass('label-success');
+        $(this).parent().addClass('label-success')
     });
 });
