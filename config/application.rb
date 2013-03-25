a23a2e79f80d491198ad68248ce223170169000c
Usman Rajab <usman.rajab@gmail.com>
2013-03-25 12:36:16 +0100

    you are part of my life

--- config/application.rb
+++ config/application.rb
@@ -64,7 +64,7 @@ module TrafikskolaApp
     # On config/application.rb forcing your application to not access the DB
     # or load models when precompiling your assets.
     config.assets.initialize_on_precompile = false
-    config.assets.compile = true
+    
 
     # Devise language selection for errors
     config.i18n.default_locale = :sv
